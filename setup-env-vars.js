1bee132f339e78dc18de9820dfba07a6a27ec3b1
vamshionrails <vamshionrails@gmail.com>
2023-11-28 23:15:16 -0600

    Create setup-env-vars.js

---
+++ setup-env-vars.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+try {
+  const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+  process.env.REPO_NAME = config.REPO_NAME;
+  process.env.DESCRIPTION = config.DESCRIPTION;
+  process.env.PRIVATE = config.PRIVATE;
+  // Add more variables as needed...
+
+  console.log('Environment variables set successfully.');
+} catch (error) {
+  console.error('Error reading or parsing the configuration file:', error);
+  process.exit(1);
+}
